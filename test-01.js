0e442e6466e95b22a7d3b2cd13a8de63833f223d
Devendra <ndevendra1310@gmail.com>
2025-08-23 20:22:46 +0530

    feat(eventloop): learnt about event loop with timers

--- test-01.js
+++ test-01.js
@@ -1,4 +1,17 @@
 import fs from "fs";
+import crypto from "crypto"
+
+/**
+ * Task 01
+ */
+
+//Heavy CPU tasks (crypto,compression,xlib)
+crypto.pbkdf2("password", "salt", 100000, 64, "sha512", () => {
+    console.log("Hasing done"); 
+});
+
+//Timers:Node js will to libuv run this after this much time libuv/os will compare with system clock runs
+setTimeout(() => console.log("Timer done"), 100);
 
 // Node js gives the tasks like I/O operations,disk,timers,DNS to libuv , which OS level API
 fs.readFile("Full_Stack_4_Month_Roadmap.pdf", "utf8", (err, data) => {
@@ -11,3 +24,22 @@ for (let i = 0; i <= 10; i++){
     sum += i;
 }
 console.log({ sum });
+
+/**
+ * Task 02
+ */
+console.log("START");
+
+setTimeout(() => {
+    console.log("setTimeout callback")
+}, 0);
+
+setImmediate(() => {
+    console.log("setImmediate Call back")
+});
+
+Promise.resolve().then(() => {
+    console.log("Promise Resolved");    
+});
+
+console.log("END");
